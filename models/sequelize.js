67dafb83e9f55ba57346ec23bea199a02a98f692
PROJINT kove1903 <kove1903@student.iepscf-uccle.be>
2020-10-25 17:14:07 +0100

    modif

--- models/sequelize.js
+++ models/sequelize.js
@@ -3,21 +3,23 @@ const debug = require("debug")("express-locallibrary-tutorial:sequelize");
 const { Sequelize, DataTypes, Model } = require("sequelize");
 
 const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  {
-    logging: (msg) => debug(msg),
-    dialect: "mysql",
-    host: config.host,
-    port: config.port,
-  }
+    config.database,
+    config.username,
+    config.password, {
+        logging: (msg) => debug(msg),
+        dialect: "mysql",
+        host: config.host,
+        port: config.port,
+    }
 );
 
-const Genre = require("./genre")(sequelize);
-const Author = require("./author")(sequelize);
-const Book = require("./book")(sequelize);
-const BookInstance = require("./bookinstance")(sequelize);
+const Genre = require("./genre")(sequelize, DataTypes);
+const Author = require("./author")(sequelize, DataTypes);
+const Book = require("./book")(sequelize, DataTypes);
+const BookInstance = require("./bookinstance")(
+    sequelize,
+    Sequelize,
+    DataTypes);
 
 Author.hasMany(Book);
 Book.belongsTo(Author);
@@ -25,5 +27,6 @@ Book.belongsToMany(Genre, { through: "bookGenres" });
 Book.hasMany(BookInstance);
 Genre.belongsToMany(Book, { through: "bookGenres" });
 BookInstance.belongsTo(Book);
+
 
 module.exports = { sequelize, Author, Book, BookInstance, Genre };
