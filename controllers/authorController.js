5a3415b6b83ddd253486953ec46556f5baec0538
PROJINT kove1903 <kove1903@student.iepscf-uccle.be>
2020-11-01 10:43:37 +0100

    suite test-cours

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,53 +1,57 @@
 const { Author, Book } = require("../models/sequelize");
 const createError = require("http-errors");
+const { body, validationResult } = require("express-validator");
 
-exports.author_list = async function (req, res, next) {
-  try {
-    const author_list = await Author.findAll({
-      order: [["family_name", "ASC"]],
-    });
-    res.render("author_list", { title: "Author List", author_list });
-  } catch (error) {
-    next(error);
-  }
+
+exports.author_list = async function(req, res, next) {
+    try {
+        const author_list = await Author.findAll({
+            order: [
+                ["family_name", "ASC"]
+            ],
+        });
+        res.render("author_list", { title: "Author List", author_list });
+    } catch (error) {
+        next(error);
+    }
 };
 
 // Display detail page for a specific Author.
-exports.author_detail = async function (req, res, next) {
-  try {
-    const authorId = req.params.id;
-    const author = await Author.findByPk(authorId, { include: Book });
-    if (author !== null) {
-      res.render("author_detail", { title: "Author Detail", author });
-    } else {
-      next(createError(404, "Author not found"));
+exports.author_detail = async function(req, res, next) {
+    try {
+        const authorId = req.params.id;
+        const author = await Author.findByPk(authorId, { include: Book });
+        if (author !== null) {
+            res.render("author_detail", { title: "Author Detail", author });
+        } else {
+            next(createError(404, "Author not found"));
+        }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 };
 
 // Display Author create form on GET.
-exports.author_create_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author create GET");
+exports.author_create_get = function(req, res, next) {
+    res.render("author_form", { title: "Create Author" });
 };
 // Handle Author create on POST.
-exports.author_create_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author create POST");
+exports.author_create_post = function(req, res) {
+    res.send("NOT IMPLEMENTED: Author create POST");
 };
 // Display Author delete form on GET.
-exports.author_delete_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author delete GET");
+exports.author_delete_get = function(req, res) {
+    res.send("NOT IMPLEMENTED: Author delete GET");
 };
 // Handle Author delete on POST.
-exports.author_delete_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author delete POST");
+exports.author_delete_post = function(req, res) {
+    res.send("NOT IMPLEMENTED: Author delete POST");
 };
 // Display Author update form on GET.
-exports.author_update_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author update GET");
+exports.author_update_get = function(req, res) {
+    res.send("NOT IMPLEMENTED: Author update GET");
 };
 // Handle Author update on POST.
-exports.author_update_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author update POST");
+exports.author_update_post = function(req, res) {
+    res.send("NOT IMPLEMENTED: Author update POST");
 };
--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -2,57 +2,57 @@ const { Book, BookInstance } = require("../models/sequelize");
 const createError = require("http-errors");
 
 // Display list of all BookInstances.
-exports.bookinstance_list = async function (req, res, next) {
-  try {
-    const bookinstance_list = await BookInstance.findAll({
-      include: Book,
-    });
-    res.render("bookinstance_list", {
-      title: "Book Instance List",
-      bookinstance_list,
-    });
-  } catch (error) {
-    next(error);
-  }
+exports.bookinstance_list = async function(req, res, next) {
+    try {
+        const bookinstance_list = await BookInstance.findAll({
+            include: Book,
+        });
+        res.render("bookinstance_list", {
+            title: "Book Instance List",
+            bookinstance_list,
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = async function (req, res, next) {
-  try {
-    const bookinstanceId = req.params.id;
-    const bookinstance = await BookInstance.findByPk(bookinstanceId, {
-      include: Book,
-    });
-    if (bookinstance !== null) {
-      res.render("bookinstance_detail", { title: "Book:", bookinstance });
-    } else {
-      next(createError(404, "Book instance not found"));
+exports.bookinstance_detail = async function(req, res, next) {
+    try {
+        const bookinstanceId = req.params.id;
+        const bookinstance = await BookInstance.findByPk(bookinstanceId, {
+            include: Book,
+        });
+        if (bookinstance !== null) {
+            res.render("bookinstance_detail", { title: "Book:", bookinstance });
+        } else {
+            next(createError(404, "Book instance not found"));
+        }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 };
 // Display BookInstance create form on GET.
-exports.bookinstance_create_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance create GET");
+exports.bookinstance_create_get = function(req, res, next) {
+    res.render("bookinstance_form", { title: "Create BookInstance" });
 };
 // Handle BookInstance create on POST.
-exports.bookinstance_create_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance create POST");
+exports.bookinstance_create_post = function(req, res) {
+    res.send("NOT IMPLEMENTED: BookInstance create POST");
 };
 // Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+exports.bookinstance_delete_get = function(req, res) {
+    res.send("NOT IMPLEMENTED: BookInstance delete GET");
 };
 // Handle BookInstance delete on POST.
-exports.bookinstance_delete_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+exports.bookinstance_delete_post = function(req, res) {
+    res.send("NOT IMPLEMENTED: BookInstance delete POST");
 };
 // Display BookInstance update form on GET.
-exports.bookinstance_update_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance update GET");
+exports.bookinstance_update_get = function(req, res) {
+    res.send("NOT IMPLEMENTED: BookInstance update GET");
 };
 // Handle bookinstance update on POST.
-exports.bookinstance_update_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance update POST");
+exports.bookinstance_update_post = function(req, res) {
+    res.send("NOT IMPLEMENTED: BookInstance update POST");
 };
