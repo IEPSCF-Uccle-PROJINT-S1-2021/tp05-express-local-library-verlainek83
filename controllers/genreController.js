06d8bf3b1011282ba14504524402349a41c06764
PROJINT kove1903 <kove1903@student.iepscf-uccle.be>
2020-11-01 10:05:33 +0100

    retest-cours

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,32 +1,21 @@
 const { Book, Genre } = require("../models/sequelize");
 const createError = require("http-errors");
-
+const { body, validationResult } = require("express-validator");
 
 // Display list of all Genre.
-exports.genre_list = async function(req, res, next) {
-    try {
-        const genre_list = await Genre.findAll();
-        res.render('genre_list', {
-            title: 'Genre_liste',
-            genre_list
-        });
-
-    } catch (error) {
-        next(error);
-    }
+exports.genre_list = function(req, res) {
+    res.send("NOT IMPLEMENTED: Genre list");
 };
-
-
 
 // Display detail page for a specific Genre.
 exports.genre_detail = async function(req, res, next) {
     try {
         const genreId = req.params.id;
         const genre = await Genre.findByPk(genreId, {
-            include: Book
+            include: Book,
         });
         if (genre !== null) {
-            res.render("genre_detail", { title: "Genre Detail", genre })
+            res.render("genre_detail", { title: "Genre Detail", genre });
         } else {
             next(createError(404, "Genre not found"));
         }
@@ -36,13 +25,35 @@ exports.genre_detail = async function(req, res, next) {
 };
 
 // Display Genre create form on GET.
-exports.genre_create_get = function(req, res) {
-    res.send("NOT IMPLEMENTED: Genre create GET");
+exports.genre_create_get = function(req, res, next) {
+    res.render("genre_form", { title: "Create Genre" });
 };
+
 // Handle Genre create on POST.
-exports.genre_create_post = function(req, res) {
-    res.send("NOT IMPLEMENTED: Genre create POST");
-};
+exports.genre_create_post = [
+    body("name", "Genre name required").trim().notEmpty().escape(),
+    async function(req, res, next) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.render("genre_form", {
+                title: "Create Genre",
+                genre: req.body,
+                errors: errors.array(),
+            });
+        } else {
+            try {
+                const [genre, created] = await Genre.findOrCreate({
+                    where: { name: req.body.name },
+                });
+                res.redirect(genre.url);
+            } catch (error) {
+                next(error);
+            }
+        }
+    },
+];
+
 // Display Genre delete form on GET.
 exports.genre_delete_get = function(req, res) {
     res.send("NOT IMPLEMENTED: Genre delete GET");
